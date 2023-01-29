7cf5660d43487ae2760f584a3449ce83a54cb465
Jina Seo <104765262+zinaisnice@users.noreply.github.com>
2023-01-29 10:01:11 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int[] solution(int n) {
+                
+        int[] answer;
+        if(n%2==0)  answer = new int[n/2];
+        else        answer = new int[n/2+1];
+        
+        int j=0;
+        for(int i=0; i<=n; i++) {
+           if(i%2==1) {
+               answer[j]=i;
+               j++;
+           }
+        }
+        
+        return answer;
+    }
+}
